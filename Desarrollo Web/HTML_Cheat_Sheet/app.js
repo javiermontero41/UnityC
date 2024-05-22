e2c761b86157f0e61d816976cf2638cbb4b268fb
javiermontero41 <a01026213@tec.mx>
2024-05-21 22:43:40 -0600

    Actividad CSS_Cheat_Sheet agregada

--- Desarrollo Web/HTML_Cheat_Sheet/app.js
+++ Desarrollo Web/HTML_Cheat_Sheet/app.js
@@ -6,9 +6,14 @@ const app = express();
 const port = 3000;
 
 app.use(express.static(path.join(__dirname, 'public')));
+app.use('/imagenes', express.static(path.join(__dirname, 'Imagenes')));
 
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'html_cheat_sheet.html'));
+    res.sendFile(path.join(__dirname, 'html', 'cheat_sheet.html'));
+});
+
+app.get('/css-cheat-sheet', (req, res) => {
+    res.sendFile(path.join(__dirname, 'html', 'css_cheat_sheet.html'));
 });
 
 app.listen(port, () => {
